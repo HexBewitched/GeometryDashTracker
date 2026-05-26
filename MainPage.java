b59875d1a6506cf799830699b6099d109c0c7241
Michael <theamazingmike111@gmail.com>
2026-05-25 23:08:25 -0500

    initial commit

---
+++ MainPage.java
@@ -0,0 +1,16 @@
+package main.java.com.zHexyy.gdTracker;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainPage
+{
+    JFrame frame;
+
+    public MainPage()
+    {
+        frame = new JFrame("GD Tracker");
+        frame.setSize(640, 480);
+        frame.setVisible(true);
+    }
+}
