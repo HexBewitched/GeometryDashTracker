9051a97e82129b44be27561eb50da349bc563e6a
hexy <theamazingmike111@gmail.com>
2026-05-27 07:56:00 -0500

    formatted project

---
+++ src/main/java/com/zHexyy/gdTracker/MainPage.java
@@ -0,0 +1,15 @@
+package main.java.com.zHexyy.gdTracker.src.main.java.com.zHexyy.gdTracker;
+
+import javax.swing.*;
+
+public class MainPage
+{
+    JFrame frame;
+
+    public MainPage()
+    {
+        frame = new JFrame("GD Tracker");
+        frame.setSize(640, 480);
+        frame.setVisible(true);
+    }
+}
---
+++ src/main/java/com/zHexyy/gdTracker/Skills.java
@@ -0,0 +1,29 @@
+package main.java.com.zHexyy.gdTracker.src.main.java.com.zHexyy.gdTracker;
+
+public enum Skills
+{
+    SHIP,
+    MEMORY,
+    WAVE,
+    BALL,
+    SPIDER,
+    CUBE,
+    SWING,
+    UFO,
+    SPAM,
+    HIGH_CPS,
+    FLOW,
+    TIMING,
+    ROBOT,
+    FAST,
+    SLOW,
+    DUALS,
+    LEARNY,
+    CHOKEPOINT,
+    OVERALL,
+    GIMMICK,
+    NERVE_CONTROL,
+    TWO_PLAYER,
+    XLLL
+
+}
